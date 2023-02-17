75ec30787401518cf2aa5db147bdd30d3f78375b
Jelani Sanders <jelanisands@gmail.com>
2023-02-17 00:26:06 -0500

    Added comments to better explain the code

--- express_server.js
+++ express_server.js
@@ -1,37 +1,45 @@
+// Initiate our express server
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-
+// Set our render engine
 app.set("view engine", "ejs");
 
+// Declare our ids with urls
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Visiting our web server's / will greet with hello
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// Log our port to console
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// urls.json returns a json object from our urlDatabase object
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// We use ejs to render our hello_world.ejs template and greet the viewer
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
 });
 
+// Displays a table of our ids and urls using our urls_index.ejs template
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// Visiting urls with urls/id returns it's longurl and renders it from our urls_show.ejs template
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
